245f5a7e7fff1e4b32f01766cfc29a1af76a9af6
vjim <vjim@SuryaNikhilaManjeeraKalla9>
2017-08-26 21:15:42 +0530

    commit by riyaz

---
+++ project/src/project/riyaz.java
@@ -0,0 +1,19 @@
+package project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+public class riyaz {
+
+	public static void main(String[] args) {
+		 WebDriver driver = new FirefoxDriver();
+		 driver.manage().window().maximize();
+		 driver.close();
+		 System.out.println("riyaz");
+
+
+	}
+
+}
